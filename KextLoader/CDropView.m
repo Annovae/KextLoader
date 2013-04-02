2ae74194321134849648be5fb284d5662bf4541f
안승례 <seungrye@gmail.com>
2013-04-02 20:54:59 +0900

    kext 파일을 드롭다운 영역에 떨궜을때, 관리자 권한을 요구하는 팝업이 뜨도록 함.

--- KextLoader/CDropView.m
+++ KextLoader/CDropView.m
@@ -8,6 +8,7 @@
 
 #import "CDropView.h"
 #import <IOKit/kext/KextManager.h>
+#import <Security/Authorization.h>
 
 @implementation CDropView
 
@@ -30,29 +31,88 @@
     return NSDragOperationGeneric;
 }
 
+- (BOOL)installKLogKext: (NSString*) sourcePath
+        DestinationPath: (NSString*) destPath
+{
+//    NSString *              sourcePath = [[NSBundle mainBundle] pathForResource:@"KLog" ofType:@"kext"];
+//    NSString *              destPath = [NSString pathWithComponents:[NSArray arrayWithObjects:@"/",@"System",@"Library",@"Extensions",@"KLog.kext",nil]];
+    NSString *              permRepairPath = [[NSBundle mainBundle] pathForResource:@"SetKLogPermissions" ofType:@"sh"];
+    
+    AuthorizationRights     myRights;
+    AuthorizationItem       myItems[1];
+    AuthorizationRef        authorizationRef;
+    OSStatus                err;
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:sourcePath] == NO) {
+        NSRunAlertPanel (@"Missing Source File", @"\"KLog.kext\" could not be installed because it is missing from the application bundle.", @"Okay", nil, nil);
+        return NO;
+    }
+    
+    myItems[0].name = kAuthorizationRightExecute;
+    myItems[0].valueLength = 0;
+    myItems[0].value = NULL;
+    myItems[0].flags = 0;
+    
+    myRights.count = sizeof(myItems) / sizeof(myItems[0]);
+    myRights.items = myItems;
+    
+    err = AuthorizationCreate (&myRights, kAuthorizationEmptyEnvironment, kAuthorizationFlagInteractionAllowed | kAuthorizationFlagExtendRights, &authorizationRef);
+    
+    if (err == errAuthorizationSuccess) {
+        char *  cpArgs[4];
+        char *  shArgs[2];
+        char *  kextloadArgs[2];
+        int     status;
+        
+        cpArgs[0] = "-r";
+        cpArgs[1] = (char *)[sourcePath cStringUsingEncoding:NSUTF8StringEncoding];
+        cpArgs[2] = (char *)[destPath cStringUsingEncoding:NSUTF8StringEncoding];
+        cpArgs[3] = NULL;
+        
+        err = AuthorizationExecuteWithPrivileges(authorizationRef, "/bin/cp", 0, cpArgs, NULL);
+        if (err) return NO;
+        
+        shArgs[0] = (char *)[permRepairPath cStringUsingEncoding:NSUTF8StringEncoding];
+        shArgs[1] = NULL;
+        
+        err = AuthorizationExecuteWithPrivileges(authorizationRef, "/bin/sh", 0, shArgs, NULL);
+        if (err) return NO;
+        
+        kextloadArgs[0] = (char *)[destPath cStringUsingEncoding:NSUTF8StringEncoding];
+        kextloadArgs[1] = NULL;
+        
+        err = AuthorizationExecuteWithPrivileges(authorizationRef, "/sbin/kextload", 0, kextloadArgs, NULL);
+        if (err) return NO;
+        
+        while (wait(&status) != -1) {
+            // wait for forked process to terminate
+        }
+        
+        AuthorizationFree(authorizationRef, kAuthorizationFlagDestroyRights);
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
 - (BOOL) performDragOperation:(id<NSDraggingInfo>)sender
 {
-    BOOL bReturn = YES;
+    BOOL bResult = YES;
     NSPasteboard* pPboard = [sender draggingPasteboard];
     
     if ([[pPboard types] containsObject: NSURLPboardType])
     {
         NSURL* pFileURL = [NSURL URLFromPasteboard: pPboard];
-        NSLog(@"file Path %@\n", [pFileURL path]);
+        NSLog(@"file Path : %@\n", [pFileURL path]);
+        NSLog(@"file Name : %@\n", [[pFileURL absoluteString] lastPathComponent]);
         
         //Load Kexts
-        CFURLRef kextUrl = (__bridge CFURLRef)pFileURL;
-        OSReturn result = KextManagerLoadKextWithURL(kextUrl, NULL);
-        CFRelease(kextUrl);
-        if (result != kOSReturnSuccess)
-        {
-            NSLog(@"Cannot load kext from %@\n", [pFileURL path]);
-            bReturn = NO;
-            goto EXIT;
-        }
+        NSString* destPath = [[NSString alloc] initWithFormat: @"/tmp/%@", [[pFileURL absoluteString] lastPathComponent]];
+        bResult = [self installKLogKext: [pFileURL path]
+                        DestinationPath: destPath];
     }
     
 EXIT:
-    return bReturn;
+    return bResult;
 }
 @end
